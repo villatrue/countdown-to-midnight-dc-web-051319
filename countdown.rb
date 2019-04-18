ee029226c60a0938da9c20208419b897b2bce4a3
Sean Carter <scarter0313@gmail.com>
2019-04-18 02:05:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,3 @@ def countdown(x)
  "HAPPY NEW YEAR!"
 end
 
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
