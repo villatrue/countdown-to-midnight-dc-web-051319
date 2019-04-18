6e1f955891a97a2b386458d96b24b664dc0f0c7b
Sean Carter <scarter0313@gmail.com>
2019-04-18 02:05:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,17 @@
 #write your code here
 
 def countdown(x)
-   while x > 1 
+   while x > 0 
   puts "#{x} SECOND(S)!"
-  x -= 
+  x -= 1
  end
  "HAPPY NEW YEAR!"
+end
+
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
